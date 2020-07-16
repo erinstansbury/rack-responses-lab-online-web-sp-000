ff54cb128006e04189ec9e43ce1fb78c9739f3d7
Erin Stansbury <ekstansbury@gmail.com>
2020-07-16 21:56:07 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application
+  
+  resp = Rack::Response.new
+  
+  def check(time)
+    if time < 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
+    end
+  end
+  
+end
